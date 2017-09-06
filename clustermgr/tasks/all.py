a311d4c04bd569b0f096b889be93a420e18e44ea
Arunmozhi <arun@arunmozhi.in>
2017-09-06 23:19:26 +0530

    refactored setup_server; removed old functions all tasks

--- clustermgr/tasks/all.py
+++ clustermgr/tasks/all.py
@@ -1,19 +1,14 @@
-import os
-import sys
 import ldap
-import StringIO
 import json
-import re
 from datetime import datetime
 
 import requests
-from fabric.api import run, execute, cd, put, env, get
-from fabric.context_managers import settings
-from fabric.contrib.files import exists
-from flask import current_app as app
 
-from clustermgr.extensions import celery, db, wlogger
-from clustermgr.models import LDAPServer, AppConfiguration, KeyRotation, \
+from fabric.api import execute, put
+from fabric.context_managers import settings
+
+from clustermgr.extensions import celery, db
+from clustermgr.models import LDAPServer, KeyRotation, \
         OxauthServer, OxelevenKeyID
 from clustermgr.core.ldaplib import ldap_conn, search_from_ldap
 from clustermgr.core.utils import decrypt_text, random_chars
@@ -23,253 +18,8 @@ from clustermgr.core.keygen import generate_jks
 ldap.set_option(ldap.OPT_X_TLS_REQUIRE_CERT, ldap.OPT_X_TLS_NEVER)
 
 
-class FabricException(Exception):
-    def __init__(self, value):
-        self.value = value
-
-    def __str__(self):
-        return self.value
-
-env.abort_exception = FabricException
-
-
 def starttls(server):
     return server.protocol == 'starttls'
-
-
-def run_command(taskid, command):
-    outlog = StringIO.StringIO()
-    errlog = StringIO.StringIO()
-
-    wlogger.log(taskid, command, "debug")
-    output = run(command, stdout=outlog, stderr=errlog, timeout=10)
-    if outlog.getvalue():
-        wlogger.log(taskid, outlog.getvalue(), "debug")
-    if errlog.getvalue():
-        wlogger.log(taskid, errlog.getvalue(), "error")
-
-    return output
-
-
-def import_ldif(taskid, ldiffile, server):
-    wlogger.log(taskid, "Copying {0} to /tmp/init.ldif".format(ldiffile),
-                "debug")
-    if server.gluu_server:
-        sloc = "gluu-server-"+server.gluu_version
-        put(ldiffile, '/opt/'+sloc+'/tmp/init.ldif')
-        run_command(taskid, chcmd(sloc, 'service solserver stop'))
-        run_command(taskid, chcmd(sloc, '/opt/symas/bin/slapadd -b o=gluu -l /tmp/init.ldif'))
-        run_command(taskid, chcmd(sloc, 'service solserver start'))
-        run_command(taskid, 'rm /opt/'+sloc+'/tmp/init.ldif')
-    else:
-        put(ldiffile, '/tmp/init.ldif')
-        run_command(taskid, 'service solserver stop')
-        run_command(taskid, '/opt/symas/bin/slapadd -b o=gluu -l /tmp/init.ldif')
-        run_command(taskid, 'service solserver start')
-        run_command(taskid, 'rm /tmp/init.ldif')
-
-
-@celery.task(bind=True)
-def initialize_provider(self, server_id, use_ldif):
-    initialized = False
-    s = LDAPServer.query.get(server_id)
-    rootuser = 'cn=directory manager,o=gluu'
-    appconfig = AppConfiguration.query.get(1)
-    repdn = appconfig.replication_dn
-    taskid = self.request.id
-    replication_user = [
-        ('objectclass', [r'person']),
-        ('cn', [r'{}'.format(
-            repdn.replace("cn=", "").replace(",o=gluu", ""))]),
-        ('sn', [r'gluu']),
-        ('userpassword', [str(appconfig.replication_pw)])
-        ]
-
-    if use_ldif:
-        host = "root@{}".format(s.hostname)
-        ldiffile = os.path.join(app.config['LDIF_DIR'],
-                                "{0}_init.ldif".format(server_id))
-        with settings(warn_only=True):
-            wlogger.log(taskid, "Importing the LDIF file")
-            execute(import_ldif, self.request.id, ldiffile, s,
-                    hosts=[host])
-
-    # Step 1: Add the Replication User DN
-    wlogger.log(taskid, 'Connecting to {}'.format(s.hostname))
-    with ldap_conn(s.hostname, s.port, rootuser, s.admin_pw, starttls(s)) \
-            as con:
-        try:
-            con.add_s(repdn, replication_user)
-            wlogger.log(taskid, 'Replication user added.', 'success')
-        except ldap.ALREADY_EXISTS:
-            con.delete_s(repdn)
-            con.add_s(repdn, replication_user)
-            wlogger.log(taskid, 'Replication user added.', 'success')
-        except ldap.LDAPError as e:
-            wlogger.log(taskid, "Failed to add Replication user", "fail")
-            print "Failed to add replication user", sys.exc_info()[2]
-            return
-    # Step 2: Reconnect as replication user
-    try:
-        con = ldap.initialize('ldap://{}:{}'.format(s.hostname, s.port))
-        if s.protocol == 'starttls':
-            con.start_tls_s()
-        con.bind_s(repdn, appconfig.replication_pw)
-        wlogger.log(taskid, "Authenticating as the Replication DN.", "success")
-        initialized = True
-    except ldap.SERVER_DOWN:
-        con = ldap.initialize('ldaps://{}:{}'.format(s.hostname, s.port))
-        con.bind_s(repdn, appconfig.replication_pw)
-        wlogger.log(taskid, "Authenticating as the Replication DN.", "success")
-        initialized = True
-    except ldap.LDAPError as e:
-        wlogger.log(taskid, "%s" % e, 'error')
-    finally:
-        con.unbind()
-
-    if initialized:
-        s.initialized = True
-        db.session.commit()
-
-
-@celery.task(bind=True)
-def replicate(self):
-    taskid = self.request.id
-    wlogger.log(taskid, 'Listing all providers')
-    providers = LDAPServer.query.filter_by(role="provider").all()
-    rootdn = "cn=directory manager,o=gluu"
-    dn = 'cn=testentry,o=gluu'
-    replication_user = [
-        ('objectclass', ['person']),
-        ('cn', ['testentry']),
-        ('sn', ['gluu']),
-        ]
-    test_result = True
-
-    wlogger.log(taskid, 'Available providers: {}'.format(len(providers)),
-                "debug")
-    for provider in providers:
-        try:
-            with ldap_conn(provider.hostname, provider.port, rootdn,
-                           provider.admin_pw, starttls(provider)) as con:
-                con.add_s(dn, replication_user)
-            wlogger.log(taskid, "Adding test data to provider {0}".format(
-                provider.hostname), "success")
-        except:
-            wlogger.log(taskid, "Adding test data to provider {0}".format(
-                provider.hostname), "fail")
-            v = sys.exc_info()[1]
-            wlogger.log(taskid, str(v), "debug")
-            test_result = test_result and False
-
-        consumers = provider.consumers
-        wlogger.log(taskid,
-                    'Listing consumers of provider %s' % provider.hostname)
-        # Check the consumers
-        for consumer in consumers:
-            wlogger.log(taskid, 'Verifying data in consumers: {} of {}'.format(
-                consumers.index(consumer)+1, len(consumers)))
-            try:
-                with ldap_conn(consumer.hostname, consumer.port, rootdn,
-                               consumer.admin_pw, starttls(consumer)) as con:
-                    if con.compare_s(dn, 'sn', 'gluu'):
-                        wlogger.log(
-                            taskid,
-                            'Test data is replicated and available', 'success')
-            except ldap.NO_SUCH_OBJECT:
-                wlogger.log(taskid, 'Test data is NOT replicated.', 'error')
-                test_result = test_result and False
-            except ldap.LDAPError as e:
-                wlogger.log(taskid, 'Failed to connect to {0}. {1}'.format(
-                                    consumer.hostname, e), 'error')
-                test_result = test_result and False
-
-        # delete the entry from the provider
-        try:
-            with ldap_conn(provider.hostname, provider.port, rootdn,
-                           provider.admin_pw, starttls(provider)) as con:
-                con.delete_s(dn)
-                if con.compare_s(dn, 'sn', 'gluu'):
-                    wlogger.log(taskid, 'Delete operation failed. Data exists',
-                                'error')
-                    test_result = test_result and False
-        except ldap.NO_SUCH_OBJECT:
-            wlogger.log(taskid, 'Deleting test data from provider: {}'.format(
-                provider.hostname), 'success')
-        except:
-            v = sys.exc_info()[1]
-            wlogger.log(taskid, str(v), "debug")
-            test_result = test_result and False
-
-        # verify the data is removed from the consumers
-        for consumer in consumers:
-            wlogger.log(
-                taskid,
-                "Verifying data is removed from consumers: {} of {}".format(
-                    consumers.index(consumer)+1, len(consumers)))
-            try:
-                with ldap_conn(consumer.hostname, consumer.port, rootdn,
-                               consumer.admin_pw, starttls(consumer)) as con:
-                    if con.compare_s(dn, 'sn', 'gluu'):
-                        wlogger.log(
-                            taskid,
-                            'Failed to remove test data in consumer {}'.format(
-                                consumer.hostname), 'error')
-                        test_result = test_result and False
-            except ldap.NO_SUCH_OBJECT:
-                    wlogger.log(
-                        taskid,
-                        'Test data removed from the consumer: {}'.format(
-                            consumer.hostname), 'success')
-            except ldap.LDAPError as e:
-                wlogger.log(
-                    taskid, 'Failed to test consumer: {0}. Error: {1}'.format(
-                        consumer.hostname, e), 'error')
-                test_result = test_result and False
-
-    wlogger.log(taskid, 'Replication test Complete.')
-    appconf = AppConfiguration.query.first()
-    appconf.last_test = test_result
-    db.session.commit()
-
-    return test_result
-
-
-def chcmd(chdir, command):
-    # chroot /chroot_dir /bin/bash -c "<command>"
-    return 'chroot /opt/{0} /bin/bash -c "{1}"'.format(chdir, command)
-
-
-def copy_certificate(server, certname):
-    certfile = os.path.join(app.config["CERTS_DIR"], certname+".crt")
-    if server.gluu_server:
-        sloc = 'gluu-server-' + server.gluu_version
-        put(certfile, "/opt/"+sloc+"/opt/symas/ssl/"+certname+".crt")
-    else:
-        put(certfile, "/opt/symas/ssl/"+certname+".crt")
-
-
-@celery.task(bind=True)
-def setup_server(self, server_id, conffile):
-    server = LDAPServer.query.get(server_id)
-    host = "root@{}".format(server.hostname)
-    tid = self.request.id
-
-    # For consumers with providers using SSL copy their certificates
-    if server.role == 'consumer' and server.provider.protocol != 'ldap':
-        with settings(warn_only=True):
-            execute(copy_certificate, server, server.provider.hostname,
-                    hosts=[host])
-    # TODO find where this copy certificate routine should be injected in
-    # cluster.py
-
-    # Everything is done. Set the flag to based on the messages
-    msgs = wlogger.get_messages(tid)
-    setup_success = True
-    for msg in msgs:
-        setup_success = setup_success and msg['level'] != 'error'
-    server.setup = setup_success
-    db.session.commit()
 
 
 def modify_oxauth_config(kr, pub_keys=None, openid_jks_pass=""):
@@ -406,6 +156,8 @@ def _rotate_keys(kr, javalibs_dir, jks_path):
         db.session.add(kr)
         db.session.commit()
 
+        # FIXME there are some fabric based command below, move them to
+        # clustermgr.core.remote.RemoteClient
         if kr.type == "jks":
             def _copy_jks(path, hostname, dest_path):
                 out = put(path, dest_path)
--- clustermgr/tasks/cluster.py
+++ clustermgr/tasks/cluster.py
@@ -76,19 +76,12 @@ def download_file(tid, c, remote, local):
 
 @celery.task(bind=True)
 def setup_server(self, server_id, conffile):
-    """This Task sets up a standalone server with only OpenLDAP installed as
-    per the request.
-
-    As the task proceeds the various status are logged to the WebLogger under
-    the uniqueID of the task. This lets the web interface to poll for the
-    near-realtime updates.
-
-    Args:
-        server_id (int): the primary key of the LDAPServer object
-        conffile (string): complete path of the slapd.conf generated via webui
-    """
     server = LDAPServer.query.get(server_id)
     tid = self.request.id
+    if server.gluu_server:
+        chdir = '/opt/gluu-server-'+server.gluu_version
+    else:
+        chdir = None
 
     wlogger.log(tid, "Connecting to the server %s" % server.hostname)
     c = RemoteClient(server.hostname)
@@ -108,147 +101,65 @@ def setup_server(self, server_id, conffile):
         wlogger.log(tid, "Ending server setup process.", "error")
         return False
 
-    wlogger.log(tid, 'Starting premilinary checks')
-    # 1. Check OpenLDAP is installed
-    if c.exists('/opt/symas/bin/slaptest'):
-        wlogger.log(tid, 'Checking if OpenLDAP is installed', 'success')
-    else:
-        wlogger.log(tid, 'Cheking if OpenLDAP is installed', 'fail')
-        wlogger.log(tid, 'Kindly install OpenLDAP on the server and refresh'
-                    ' this page to try setup again.')
-        return
-
-    # 2. symas-openldap.conf file exists
-    if c.exists('/opt/symas/etc/openldap/symas-openldap.conf'):
-        wlogger.log(tid, 'Checking symas-openldap.conf exists', 'success')
-    else:
-        wlogger.log(tid, 'Checking if symas-openldap.conf exists', 'fail')
-        wlogger.log(tid, 'Configure OpenLDAP with /opt/gluu/etc/openldap'
-                    '/symas-openldap.conf', 'warning')
-        return
-
-    # 3. Certificates
-    if server.tls_cacert:
-        if c.exists(server.tls_cacert):
-            wlogger.log(tid, 'Checking TLS CA Certificate', 'success')
+    # Premilinary checks for standalone servers with openldap installed
+    if not server.gluu_server:
+        # 1. Check OpenLDAP is installed
+        if c.exists('/opt/symas/bin/slaptest'):
+            wlogger.log(tid, 'Checking if OpenLDAP is installed', 'success')
         else:
-            wlogger.log(tid, 'Checking TLS CA Certificate', 'fail')
-    if server.tls_servercert:
-        if c.exists(server.tls_servercert):
-            wlogger.log(tid, 'Checking TLS Server Certificate', 'success')
+            wlogger.log(tid, 'Cheking if OpenLDAP is installed', 'fail')
+            wlogger.log(tid, 'Kindly install OpenLDAP on the server and '
+                        ' refresh this page to try setup again.')
+            return
+        # 2. symas-openldap.conf file exists
+        if c.exists('/opt/symas/etc/openldap/symas-openldap.conf'):
+            wlogger.log(tid, 'Checking symas-openldap.conf exists', 'success')
         else:
-            wlogger.log(tid, 'Checking TLS Server Certificate', 'fail')
-    if server.tls_serverkey:
-        if c.exists(server.tls_serverkey):
-            wlogger.log(tid, 'Checking TLS Server Key', 'success')
-        else:
-            wlogger.log(tid, 'Checking TLS Server Key', 'fail')
+            wlogger.log(tid, 'Checking if symas-openldap.conf exists', 'fail')
+            wlogger.log(tid, 'Configure OpenLDAP with /opt/gluu/etc/openldap'
+                        '/symas-openldap.conf', 'warning')
+            return
 
-    # 4. Data directories
-    wlogger.log(tid, "Checking for data and schema folders for LDAP")
-    conf = open(conffile, 'r')
-    for line in conf:
-        if re.match('^directory', line):
-            folder = line.split()[1]
-            if not c.exists(folder):
-                run_command(tid, c, 'mkdir -p '+folder)
+        # 3. Certificates
+        if server.tls_cacert:
+            if c.exists(server.tls_cacert):
+                wlogger.log(tid, 'Checking TLS CA Certificate', 'success')
             else:
-                wlogger.log(tid, folder, 'success')
+                wlogger.log(tid, 'Checking TLS CA Certificate', 'fail')
+        if server.tls_servercert:
+            if c.exists(server.tls_servercert):
+                wlogger.log(tid, 'Checking TLS Server Certificate', 'success')
+            else:
+                wlogger.log(tid, 'Checking TLS Server Certificate', 'fail')
+        if server.tls_serverkey:
+            if c.exists(server.tls_serverkey):
+                wlogger.log(tid, 'Checking TLS Server Key', 'success')
+            else:
+                wlogger.log(tid, 'Checking TLS Server Key', 'fail')
 
-    # 5. Copy Gluu Schema files
-    wlogger.log(tid, "Copying Schema files to server")
-    if not c.exists('/opt/gluu/schema/openldap'):
-        run_command(tid, c, 'mkdir -p /opt/gluu/schema/openldap')
-    gluu_schemas = os.listdir(os.path.join(app.static_folder, 'schema'))
-    for schema in gluu_schemas:
-        upload_file(tid, c, os.path.join(app.static_folder, 'schema', schema),
-                    "/opt/gluu/schema/openldap/"+schema)
-    # 6. Copy User's custom schema files
-    schemas = os.listdir(app.config['SCHEMA_DIR'])
-    for schema in schemas:
-        upload_file(tid, c, os.path.join(app.config['SCHEMA_DIR'], schema),
-                    "/opt/gluu/schema/openldap/"+schema)
-
-    # 7. Setup slapd.conf
-    wlogger.log(tid, "Copying slapd.conf file to remote server")
-    upload_file(tid, c, conffile, '/opt/symas/etc/openldap/slapd.conf')
-
-    wlogger.log(tid, "Restarting LDAP server to validate slapd.conf")
-    # IMPORTANT:
-    # Restart allows the server to create missing mdb files for accesslog so
-    # slapd.conf -> slapd.d conversion runs without error
-    run_command(tid, c, 'service solserver restart')
-
-    # 8. Generate OLC slapd.d
-    wlogger.log(tid, "Migrating from slapd.conf to slapd.d OnlineConfig (OLC)")
-    run_command(tid, c, 'service solserver stop')
-    run_command(tid, c, 'rm -rf /opt/symas/etc/openldap/slapd.d')
-    run_command(tid, c, 'mkdir -p /opt/symas/etc/openldap/slapd.d')
-    run_command(tid, c,
-                '/opt/symas/bin/slaptest -f /opt/symas/etc/openldap/slapd.conf'
-                ' -F /opt/symas/etc/openldap/slapd.d')
-
-    # 9. Restart the solserver with the new configuration
-    wlogger.log(tid, "Starting LDAP server with OLC configuraion. Any future"
-                "changes to slapd.conf will have NO effect on the LDAP server")
-    log = run_command(tid, c, 'service solserver start')
-    if 'failed' in log:
-        wlogger.log(tid, "OpenLDAP server failed to start.", "error")
-        wlogger.log(tid, "Debugging slapd...", "info")
-        run_command(tid, "service solserver start -d 1")
-
-    # Everything is done. Set the flag to based on the messages
-    msgs = wlogger.get_messages(tid)
-    setup_success = True
-    for msg in msgs:
-        setup_success = setup_success and msg['level'] != 'error'
-    server.setup = setup_success
-    db.session.commit()
-
-
-
-@celery.task(bind=True)
-def configure_gluu_server(self, server_id, conffile):
-    server = LDAPServer.query.get(server_id)
-    tid = self.request.id
-    chdir = '/opt/gluu-server-'+server.gluu_version
-
-    wlogger.log(tid, "Connecting to the server %s" % server.hostname)
-    c = RemoteClient(server.hostname)
-    try:
-        c.startup()
-    except Exception as e:
-        wlogger.log(tid, "Cannot establish SSH connection {0}".format(e),
-                    "error")
-
-    wlogger.log(tid, "Retrying with the IP address")
-    c = RemoteClient(server.ip)
-    try:
-        c.startup()
-    except Exception as e:
-        wlogger.log(tid, "Cannot establish SSH connection {0}".format(e),
-                    "error")
-        wlogger.log(tid, "Ending server setup process.", "error")
-        return False
-
-    # Since it is a Gluu Server, a number of checks can be avoided
-    # 1. Check if OpenLDAP is installed
-    # 2. Check if symas-openldap.conf files exists
-    # 3. Check for certificates - They will be at /etc/certs
-
-    # 4. Existance of data directories - this is necassr check as we will be
-    #    enabling accesslog DIT, maybe others by admin in the conf editor
+    # 4. Check for existance of all the directories, create them if they don't
     wlogger.log(tid, "Checking existing data and schema folders for LDAP")
     conf = open(conffile, 'r')
     for line in conf:
         if re.match('^directory', line):
             folder = line.split()[1]
-            if not c.exists(os.path.join(chdir, folder)):
+            if server.gluu_server:
+                folder = os.path.join(chdir, folder)
+            if not c.exists(folder):
                 run_command(tid, c, 'mkdir -p '+folder, chdir)
             else:
                 wlogger.log(tid, folder, 'success')
 
-    # 5. Gluu Schema file will be present - no checks required
+    # 5. Gluu Schema file will be present - Only for Standalone
+    if not server.gluu_server:
+        wlogger.log(tid, "Copying Gluu Schema files to the server")
+        if not c.exists('/opt/gluu/schema/openldap'):
+            run_command(tid, c, 'mkdir -p /opt/gluu/schema/openldap')
+        gluu_schemas = os.listdir(os.path.join(app.static_folder, 'schema'))
+        for schema in gluu_schemas:
+            upload_file(tid, c,
+                        os.path.join(app.static_folder, 'schema', schema),
+                        "/opt/gluu/schema/openldap/"+schema)
 
     # 6. Copy User's custom schema files if any
     schemas = os.listdir(app.config['SCHEMA_DIR'])
@@ -256,12 +167,17 @@ def configure_gluu_server(self, server_id, conffile):
         wlogger.log(tid, "Copying custom schema files to the server")
         for schema in schemas:
             local = os.path.join(app.config['SCHEMA_DIR'], schema)
-            remote = chdir+"/opt/gluu/schema/openldap/"+schema
+            remote = "/opt/gluu/schema/openldap/"+schema
+            if chdir:
+                remote = chdir+"/opt/gluu/schema/openldap/"+schema
             upload_file(tid, c, local, remote)
 
     # 7. Copy the slapd.conf
     wlogger.log(tid, "Copying slapd.conf file to the server")
-    upload_file(tid, c, conffile, chdir+"/opt/symas/etc/openaldap/slapd.conf")
+    remote = "/opt/symas/etc/openldap/slapd.conf"
+    if chdir:
+        remote = chdir + "/opt/symas/etc/openldap/slapd.conf"
+    upload_file(tid, c, conffile, remote)
 
     wlogger.log(tid, "Restarting LDAP server to validate slapd.conf")
     # IMPORTANT:
@@ -270,11 +186,15 @@ def configure_gluu_server(self, server_id, conffile):
     run_command(tid, c, 'service solserver restart', chdir)
 
     # 8. Download openldap.crt to be used in other servers for ldaps
-    wlogger.log(tid, "Downloading SSL Certificate to be used in other servers")
-    remote = chdir + '/etc/certs/openldap.crt'
-    local = os.path.join(app.config["CERTS_DIR"],
-                         "{0}.crt".format(server.hostname))
-    download_file(tid, c, remote, local)
+    if server.gluu_server or server.protocol == 'ldaps':
+        wlogger.log(tid, "Downloading SSL Certificate to use in other servers")
+        if server.gluu_server:
+            remote = chdir + '/etc/certs/openldap.crt'
+        else:
+            remote = server.tls_servercert
+        local = os.path.join(app.config["CERTS_DIR"],
+                             "{0}.crt".format(server.hostname))
+        download_file(tid, c, remote, local)
 
     # 9. Generate OLC slapd.d
     wlogger.log(tid, "Convert slapd.conf to slapd.d OLC")
