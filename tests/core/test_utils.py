bd9a4a6e8881215b96db399d6fc6a0ea3809286b
Arunmozhi <arun@arunmozhi.in>
2017-10-01 01:16:18 +0530

    adds test cases for some core.utils functions

--- tests/core/test_utils.py
+++ tests/core/test_utils.py
@@ -1,7 +1,9 @@
 import os
 import unittest
 
-from clustermgr.core.utils import parse_slapdconf
+from mock import patch
+
+from clustermgr.core.utils import parse_slapdconf, ldap_encode, generate_random_key
 
 
 class SlapdConfParseTest(unittest.TestCase):
@@ -21,3 +23,32 @@ class SlapdConfParseTest(unittest.TestCase):
                           "{SSHA}NtdgEfn/RjKonrJcvi2Qqn4qrk8ccedb")
         self.assertEquals(values["BCRYPT"], "{BCRYPT}")
 
+
+class LDAPEncodeTestCase(unittest.TestCase):
+    def test_ldap_encode(self):
+        assert "{SSHA}" in ldap_encode('A Password')
+
+    @patch('clustermgr.core.utils.os.urandom')
+    def test_ldap_encode_uses_a_random_salt(self, mockur):
+        mockur.return_value = 'asdf'
+        ldap_encode('password')
+        mockur.assert_called_once_with(4)
+
+
+class GenerateRandomKeyTestCase(unittest.TestCase):
+    def test_gen_rand_key_returns_random_string_of_requested_length(self):
+        key = generate_random_key(10)
+        assert len(key) == 10
+
+    def test_gen_rand_key_returns_a_default_length_of_32(self):
+        assert len(generate_random_key()) == 32
+
+    @patch('clustermgr.core.utils.os.urandom')
+    def test_gen_rand_key_uses_os_urandom(self, mockur):
+        mockur.return_value = 'asdf'
+        generate_random_key(10)
+        mockur.assert_called_once_with(10)
+
+
+if __name__ == "__main__":
+    unittest.main()
