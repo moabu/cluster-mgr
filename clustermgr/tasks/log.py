e38cdc434f8f036e1d9eac83fe3fe6457f796a6b
Isman Firmansyah <isman.firmansyah@gmail.com>
2018-01-23 03:21:45 +0700

    basic component setup, filtering, and collecting logs

--- clustermgr/tasks/log.py
+++ clustermgr/tasks/log.py
@@ -1,10 +1,15 @@
 import json
 
 from celery.utils.log import get_task_logger
+from flask import current_app
+from flask import render_template
 from influxdb import InfluxDBClient
 
 from ..core.remote import RemoteClient
 from ..extensions import celery
+from ..extensions import wlogger
+from ..models import AppConfiguration
+from ..models import Server
 
 task_logger = get_task_logger(__name__)
 
@@ -14,21 +19,19 @@ def _filebeat_to_influx(log):
     #
     #     {
     #         u'beat': {u'hostname': u'gluu-elk', u'name': u'gluu-elk', u'version': u'5.6.3'},
-    #         u'fields': {u'ip': u'172.40.40.40', u'gluu': {u'chroot': True, u'version': u'3.1.1'}, u'os': u'Ubuntu 14.04'},
+    #         u'fields': {u'ip': u'172.40.40.40', u'gluu': {u'chroot': True, u'version': u'3.1.1'}, u'os': u'Ubuntu 14.04', u'type': u'httpd'},
     #         u'@timestamp': u'2018-01-19T15:09:12.096Z',
     #         u'source': u'/var/log/apache2/access.log',
     #         u'offset': 972,
     #         u'input_type': u'log',
     #         u'message': u'::1 - - [19/Jan/2018:15:09:11 +0000] "GET /g HTTP/1.1" 404 433 "-" "curl/7.35.0"',
-    #         u'type': u'httpd',
     #     }
     _log = {}
     _log["time"] = log["@timestamp"]
     _log["measurement"] = "logs"
     _log["tags"] = log["fields"]
     _log["tags"]["hostname"] = log["beat"]["hostname"]
-    _log["fields"] = {k: v for k, v in log.iteritems()
-                      if k in ("message", "source", "type")}
+    _log["fields"] = {k: v for k, v in log.iteritems() if k in ("message", "source")}
     return _log
 
 
@@ -56,11 +59,137 @@ def collect_logs(host, ip, path, influx_fmt=True):
         logs = filter(None, [parse_log(log) for log in stdout.splitlines()])
 
     rc.close()
-    return logs
 
-
-@celery.task
-def save_logs(logs, db_host="localhost", db_port=8086, db_name="gluu_logs"):
-    influx = InfluxDBClient(host=db_host, port=db_port, database=db_name)
-    task_logger.info(logs)
+    influx = InfluxDBClient(database="gluu_logs")
+    influx.create_database("gluu_logs")
     return influx.write_points(logs)
+
+
+def _install_filebeat(task_id, server, rc):
+    """Installs filebeat.
+
+    Docs at https://www.elastic.co/guide/en/beats/filebeat/current/filebeat-installation.html.
+    """
+    stdout = ""
+    stderr = ""
+
+    cmd_list = [
+        # 1) install apt transport
+        "DEBIAN_FRONTEND=noninteractive apt-get install -y apt-transport-https",
+        # 2) fetch public signing key
+        "wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | apt-key add -",
+        # 3) add repo definition
+        "echo 'deb https://artifacts.elastic.co/packages/6.x/apt stable main' | tee /etc/apt/sources.list.d/elastic-6.x.list",
+        # 4) install filebeat
+        "DEBIAN_FRONTEND=noninteractive apt-get update && apt-get install -y filebeat",
+        # 5) startup script
+        "update-rc.d filebeat defaults 95 10",
+    ]
+
+    for cmd in cmd_list:
+        wlogger.log(task_id, cmd, "info", server_id=server.id)
+        _, stdout, stderr = rc.run(cmd)
+        if stderr:
+            return stdout, stderr
+    return stdout, stderr
+
+
+def _render_filebeat_config(task_id, server, rc):
+    # render filebeat.yml and upload to server
+    appconf = AppConfiguration.query.first()
+
+    with current_app.app_context():
+        ctx = {
+            "ip": server.ip,
+            "os": server.os,
+            "chroot": "true" if server.gluu_server is True else "",
+            "chroot_path": "opt/gluu-server-{}".format(appconf.gluu_version) if server.gluu_server else "",
+            "gluu_version": appconf.gluu_version,
+        }
+        txt = render_template("filebeat/filebeat.yml", **ctx)
+        wlogger.log(task_id, "uploading filebeat.yml to remote server", "info", server_id=server.id)
+        status, maybe_err = rc.put_file("/etc/filebeat/filebeat.yml", txt)
+        return status, maybe_err
+
+
+def _restart_filebeat(task_id, server, rc):
+    cmd = "service filebeat restart"
+    wlogger.log(task_id, cmd, "info", server_id=server.id)
+    _, stdout, stderr = rc.run(cmd)
+    return stdout, stderr
+
+
+@celery.task(bind=True)
+def setup_components(self):
+    tid = self.request.id
+    servers = Server.query.all()
+
+    for server in servers:
+        # establishes SSH connection
+        wlogger.log(
+            tid,
+            "Making SSH connection to the server {}.".format(server.hostname),
+            "info",
+            server_id=server.id,
+        )
+
+        rc = RemoteClient(server.hostname, server.ip)
+        try:
+            rc.startup()
+
+            # installs filebeat
+            _, stderr = _install_filebeat(tid, server, rc)
+            if stderr:
+                wlogger.log(
+                    tid,
+                    "Cannot install filebeat component; reason={}".format(stderr),
+                    "warning",
+                    server_id=server.id,
+                )
+
+            # renders filebeat config
+            uploaded, stderr = _render_filebeat_config(tid, server, rc)
+            if not uploaded:
+                wlogger.log(
+                    tid,
+                    "Cannot render/upload filebeat.yml; reason={}".format(stderr),
+                    "warning",
+                    server_id=server.id,
+                )
+
+            # restarts filebeat service
+            # note, restarting filebeat service may gives unwanted output,
+            # hence we skip checking the result of running command
+            _restart_filebeat(tid, server, rc)
+        except Exception as exc:
+            wlogger.log(
+                tid,
+                "Cannot establish SSH connection {}".format(exc),
+                "warning",
+                server_id=server.id,
+            )
+            wlogger.log(
+                tid,
+                "Ending server setup process.",
+                "error",
+                server_id=server.id,
+            )
+            return False
+        finally:
+            rc.close()
+    return True
+
+
+@celery.task(bind=True)
+def setup_influxdb(self):
+    tid = self.request.id
+
+    wlogger.log(
+        tid,
+        "Creating InfluxDB database gluu_logs",
+        "info",
+    )
+
+    influx = InfluxDBClient(database="gluu_logs")
+    influx.create_database("gluu_logs")
+    return True
--- clustermgr/views/logserver.py
+++ clustermgr/views/logserver.py
@@ -2,20 +2,25 @@
 the logging server managed in the cluster-manager
 """
 
-from celery import chord
 from flask import Blueprint
 from flask import render_template
 from flask import request
 from flask import current_app
+from flask import flash
+from flask import redirect
+from flask import url_for
 from influxdb import InfluxDBClient
 from influxdb.exceptions import InfluxDBClientError
+from requests.exceptions import ConnectionError
 
 from ..core.license import license_reminder
 from ..forms import LogSearchForm
 from ..models import Server
+from ..models import AppConfiguration
 
 from ..tasks.log import collect_logs
-from ..tasks.log import save_logs
+from ..tasks.log import setup_components
+from ..tasks.log import setup_influxdb
 
 log_mgr = Blueprint('log_mgr', __name__)
 log_mgr.before_request(license_reminder)
@@ -23,6 +28,7 @@ log_mgr.before_request(license_reminder)
 
 def search_by_filters(type_="", message="", host="", limit=25, offset=0):
     influx = InfluxDBClient(database="gluu_logs")
+    influx.create_database("gluu_logs")
 
     # queryset
     qs = ["SELECT * FROM logs"]
@@ -31,11 +37,13 @@ def search_by_filters(type_="", message="", host="", limit=25, offset=0):
     if type_:
         tags["type"] = type_.lower()
     if host:
-        tags["hostname"] = host
+        # IP is chosen because filebeat strips dotted hostname
+        tags["ip"] = host
     if message:
         qs.append("WHERE message =~ /{}/".format(message))
 
     qs.append("ORDER BY time DESC")
+    # @TODO: pagination
     qs.append("LIMIT {}".format(limit))
     qs.append("OFFSET {}".format(offset))
 
@@ -45,13 +53,13 @@ def search_by_filters(type_="", message="", host="", limit=25, offset=0):
 
 @log_mgr.route("/")
 def index():
+    err = ""
+    logs = []
+
     # populate host drop-down
     servers = [("", "")]
     for server in Server.query:
-        servers.append((server.hostname, server.hostname))
-    servers.append(("gluu-elk", "gluu-elk"))  # dummy
-
-    err = ""
+        servers.append((server.ip, "{}/{}".format(server.hostname, server.ip)))
 
     form = LogSearchForm()
     form.host.choices = servers
@@ -65,22 +73,59 @@ def index():
             message=form.message.data,
             host=form.host.data,
         )
-    except InfluxDBClientError as exc:
-        err = exc
-        current_app.logger.info(exc)
-        logs = []
+    except (InfluxDBClientError, ConnectionError) as exc:
+        err = "Unable to connect to InfluxDB"
+        current_app.logger.info("{}; reason={}".format(err, exc))
     return render_template("log_index.html", form=form, logs=logs, err=err)
 
 
-@log_mgr.route("/setup/")
-def setup():
-    return "Logging Setup"
+@log_mgr.route("/setup_remote/")
+def setup_remote():
+    # checks for existing app config
+    appconf = AppConfiguration.query.first()
+
+    if not appconf:
+        flash("The application needs to be configured first. Kindly set the "
+              "values before attempting clustering.", "warning")
+        return redirect(url_for("index.app_configuration"))
+
+    # checks for existing servers
+    servers = Server.query.all()
+
+    if not servers:
+        flash("Add servers to the cluster before attempting to manage logs",
+              "warning")
+        return redirect(url_for('index.home'))
+
+    task = setup_components.delay()
+    return render_template("log_setup.html", step=1,
+                           task_id=task.id, servers=servers)
+
+
+@log_mgr.route("/setup_local/")
+def setup_local():
+    # checks for existing app config
+    appconf = AppConfiguration.query.first()
+
+    if not appconf:
+        flash("The application needs to be configured first. Kindly set the "
+              "values before attempting clustering.", "warning")
+        return redirect(url_for("index.app_configuration"))
+
+    # checks for existing servers
+    servers = [Server(id=0, hostname="localhost")]
+
+    if not servers:
+        flash("Add servers to the cluster before attempting to manage logs",
+              "warning")
+        return redirect(url_for('index.home'))
+
+    task = setup_influxdb.delay()
+    return render_template("log_setup.html", task_id=task.id, step=2, servers=servers)
 
 
 @log_mgr.route("/collect/")
 def collect():
-    servers = ["172.40.40.40"]
-    task = chord(collect_logs.s(server, server, "/tmp/gluu-filebeat")
-                 for server in servers)(save_logs.s())
-    task.get()
-    return "Collecting Logs"
+    for server in Server.query:
+        collect_logs.delay(server.hostname, server.ip, "/tmp/gluu-filebeat")
+    return redirect(url_for(".index"))
