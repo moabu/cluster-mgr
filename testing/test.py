73a93255a6924b1b314898c763a3cd27a1b3541b
Mustafa Başer <mbaser@mail.com>
2018-02-22 01:37:22 +0300

    change name of hostname changer and make it importable

---
+++ testing/test.py
@@ -0,0 +1,21 @@
+from change_gluu_host import Installer, FakeRemote, ChangeGluuHostname
+
+name_changer = ChangeGluuHostname(
+    old_host='c1.gluu.org',
+    new_host='c3.gluu.org',
+    cert_city='MyCity',
+    cert_mail='admin@gluu.org',
+    cert_state='NA',
+    cert_country='US',
+    server='c1.gluu.org',
+    ldap_password='topsecret',
+    os_type='Ubuntu'
+    )
+    
+name_changer.startup()
+name_changer.change_appliance_config()
+name_changer.change_clients()
+name_changer.change_uma()
+name_changer.change_httpd_conf()
+name_changer.create_new_certs()
+name_changer.change_host_name()
