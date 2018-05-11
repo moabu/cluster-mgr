37091a51b5c4767c4328d71f493eda727aaac5fb
Mustafa Başer <mbaser@mail.com>
2018-05-11 16:10:39 +0300

    move cluster.mgr to home directory

--- clustermgr/config.py
+++ clustermgr/config.py
@@ -19,7 +19,7 @@ class Config(object):
         "DATA_DIR",
         os.path.join(os.path.expanduser("~"), ".clustermgr"),
     )
-    LOG_FILE = os.path.join(DATA_DIR, 'clustermgr.log')
+    LOG_FILE = os.path.join(os.path.expanduser("~"), 'clustermgr.log')
     JAVALIBS_DIR = os.path.join(DATA_DIR, "javalibs")
     APP_INSTANCE_DIR = os.path.join(DATA_DIR, "instance")
     SCHEMA_DIR = os.path.join(DATA_DIR, "schema")
