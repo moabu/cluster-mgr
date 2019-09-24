a95376e3c103208f8a518cf624849aa3b10059f2
Mustafa Baser <mbaser@mail.com>
2019-09-24 22:08:02 +0300

    remove slapd parser

--- clustermgr/core/utils.py
+++ clustermgr/core/utils.py
@@ -38,54 +38,6 @@ logger.setLevel(logging.ERROR)
 logger.addHandler(handler)
 
 
-def parse_slapdconf(old_conf=None):
-    """Parses the slapd.conf file generated during the installation of
-    Gluu server and gets the values necessary for the provider.conf.
-
-    Args:
-        old_conf (string) - OPTIONAL Location of the slapd.conf file.
-            Defatuls to /opt/symas/etc/openldap/slapd.conf
-
-    Returns:
-        dict containing the values for the following
-            * openldapSchemaFolder
-            * openldapTLSCACert
-            * openldapTLSCert
-            * openldapTLSKey
-            * encoded_ldap_pw
-            * BCRYPT - This has {} around it, so an escape value `{BCRYPT}`
-    """
-    if not old_conf:
-        old_conf = '/opt/symas/etc/openldap/slapd.conf'
-
-    f = open(old_conf, 'r')
-    values = {}
-
-    for line in f:
-        # openldapSchemaFolder
-        if 'gluu.schema' in line and re.match('^include*', line):
-            path = line.split("\"")[1].replace("/gluu.schema", "")
-            values["openldapSchemaFolder"] = path
-        # openldapTLSCACert
-        if re.match("^TLSCACertificateFile*", line):
-            values["openldapTLSCACert"] = line.split("\"")[1]
-        # openldapTLSCert
-        if re.match("^TLSCertificateFile*", line):
-            values["openldapTLSCert"] = line.split("\"")[1]
-        # openldapTLSKey
-        if re.match("^TLSCertificateKeyFile*", line):
-            values["openldapTLSKey"] = line.split("\"")[1]
-        # encoded_ldap_pw
-        if re.match("^rootpw", line):
-            values["encoded_ldap_pw"] = line.split()[1]
-    f.close()
-
-    # BCRYPT - This has {} around it so escape this
-    values["BCRYPT"] = "{BCRYPT}"
-
-    return values
-
-
 def ldap_encode(password):
     salt = os.urandom(4)
     sha = hashlib.sha1(password)
@@ -218,7 +170,7 @@ def get_quad():
 
 def get_inums():
     """This fuction created inums based on Python's uuid4 function.
-    Barrowed from setup.py of gluu installer"""
+    Barrowed from setup.pmastery of gluu installer"""
 
     base_inum = '@!%s.%s.%s.%s' % tuple([get_quad() for _ in xrange(4)])
     org_two_quads = '%s.%s' % tuple([get_quad() for _ in xrange(2)])
