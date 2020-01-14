23af636d1af017fb5e86c4c3721e61aa49e256d3
Mustafa Baser <mbaser@mail.com>
2020-01-14 10:49:10 +0300

    downgrade billiard to 3.5.0.4

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ setup(
         "more_itertools==5.0.0",
         "MarkupSafe>=0.23",
         "kombu==4.5",
-        "billiard==3.6",
+        "billiard==3.5.0.4",
         "cffi",
         "asn1crypto",
         "blinker",
