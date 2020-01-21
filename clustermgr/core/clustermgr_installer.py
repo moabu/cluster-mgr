39b0a9d95695c92618ec2c7d5249cc24d9e94527
Mustafa Baser <mbaser@mail.com>
2020-01-21 19:58:47 +0300

    add yum clean all

--- clustermgr/core/clustermgr_installer.py
+++ clustermgr/core/clustermgr_installer.py
@@ -334,6 +334,7 @@ class Installer:
 
         if not self.repo_updated[inside]:
             if self.clone_type == 'rpm':
+                self.run('yum clean all', inside)
                 self.run('yum repolist', inside)
             else:
                 self.run('DEBIAN_FRONTEND=noninteractive apt-get update', inside)
