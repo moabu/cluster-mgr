c0a41ac5d0abea2c1c805d546cf3d195ee99ee6d
Mustafa Baser <mbaser@mail.com>
2020-04-07 19:55:07 +0300

    specify pyasn1-modules version

--- setup.py
+++ setup.py
@@ -59,7 +59,7 @@ setup(
         "influxdb==5.0.0",
         'gunicorn==19.7.1',
         'psutil',
-        'pyasn1-modules',
+        'pyasn1-modules==0.4.8',
     ],
     scripts=['clusterapp.py', 'clustermgr4-cli'],
     classifiers=[
