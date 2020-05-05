92fdd0b4f25bc6156e929e5721a4ed24e5916a11
Mustafa Baser <mbaser@mail.com>
2020-05-05 15:12:52 +0300

    closes #298 enable installaing redis on CentOS

--- clustermgr/tasks/cache.py
+++ clustermgr/tasks/cache.py
@@ -163,7 +163,12 @@ def install_cache_cluster(self, servers_id_list, cache_servers_id_list):
 
         redis_package = 'redis-server' if installer.clone_type == 'deb' else 'redis'  
         if not redis_installed:            
+
+            if installer.clone_type == 'rpm':
+                installer.epel_release()
+            
             wlogger.log(task_id, "Installing Redis Server", "info", server_id=server.id)
+            
             
             installer.install(redis_package, inside=False)
             
